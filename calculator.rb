57db057defc427940d2e65ca63eedcb875e775ec
Erik Kasperson <chefkasperson@gmail.com>
2019-11-19 04:58:30 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 # Add your variables here
 first_number = 7
-second_number = 0
+second_number = 2.4
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
